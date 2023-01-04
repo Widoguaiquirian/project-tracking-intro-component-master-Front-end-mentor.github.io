c1f22004b253f2559eadcd9a5d188ccdf90b5c3b
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2023-01-04 06:59:08 -0300

    Adding mobile nav

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,11 @@
 "use strict";
+const menuBtn = document.querySelector(".menu__btn");
+const navMobile = document.querySelector(".nav__mobile");
+const btnHamburguer = document.querySelector(".btnHamburguer");
+const btnCloseMenu = document.querySelector(".btnClose");
+
+menuBtn.addEventListener("click", function () {
+   navMobile.classList.toggle("active");
+   btnCloseMenu.classList.toggle("active");
+   btnHamburguer.classList.toggle("active");
+});
